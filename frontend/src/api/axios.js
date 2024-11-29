df4a927bf6f29fd6e23d3a09e8ee39c2dee5a8d1
Aadcode <118838917+Aadcode@users.noreply.github.com>
2024-11-29 10:49:17 +0530

    package.json modified

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://rbac-u5ha.onrender.com/api',
 });
 
 api.interceptors.request.use(
